0722eecbedf032d7e92f640804a1758c74eaa247
Shirley Leu <shirley.leu@schibsted.com>
2018-11-17 14:56:33 +0100

    Create template

--- main/main.go
+++ main/main.go
@@ -2,30 +2,56 @@ package main
 
 import (
 	"fmt"
-	"github.com/shirleyleu/cyoa/storymaker"
+	"github.com/shirleyleu/cyoa/cyoa"
+	"html/template"
 	"log"
 	"net/http"
 )
 
+var tmpl = template.Must(template.New("CYOA").Parse(`
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Choose Your Own Adventure</title>
+</head>
+<body>
+    <h1>{{.Title}}</h1>
+    {{range .Paragraphs}}
+    <p>{{.}}</p>
+    {{end}}
+    <ul>
+        {{range .Options}}
+        <li><a href="/{{.Arc}}">{{.OptionText}}</a></li>
+        {{end}}
+    </ul>
+</body>
+</html>`))
+
 type storyHandler struct{
-	story storymaker.Story
+	story cyoa.Story
 }
 
-func (s storyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (h storyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	dest := r.URL.Path[1:]
 	if dest == "" {
 		dest = "intro"
 	}
-	v, ok := s.story[dest]
+	v, ok := h.story[dest]
 	if !ok {
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
-	fmt.Println(v)
+	err := tmpl.Execute(w, v)
+	if err !=nil {
+		log.Printf("tmpl.Execute: %s", err)
+		return
+	}
 }
 
 func main() {
-	m, err := storymaker.ParseJSON("../gopher.json")
+	m, err := cyoa.ParseJSON("../gopher.json")
 	if err != nil {
 		log.Fatalf("Unable to parse JSON: %s", err)
 	}
@@ -33,8 +59,3 @@ func main() {
 	fmt.Println("Listening on http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
-
-
-
-
-// Create HTML index that starts with intro story
